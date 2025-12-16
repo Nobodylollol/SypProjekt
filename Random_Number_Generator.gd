9cc0614bd068dec355166cb10d2f343ed06ef2e4
Nobodylollol <nicoposchner@gmail.com>
2025-12-16 14:48:51 +0100

    Implemented Button & textfield

--- Random_Number_Generator.gd
+++ Random_Number_Generator.gd
@@ -1,17 +1,21 @@
 extends Node
-
 var rng = RandomNumberGenerator.new()
-var Result = 0;
+var Result;
 var Spin_Button;
+var NumCon = NumberConnection.new();
+var NumberEntered = NumCon.getNumEntered()
+
 
 func _ready():
 	Spin_Button = Button.new()
-	Spin_Button.pressed.connect(_SpinButton)
 	add_child(Spin_Button)
-	
+	print("Button Ready")
 	
 
-func _SpinButton():
+func _on_button_down() -> int:
 	Result = rng.randi_range(0,36)
-	Spin_Button.text = Result
+	print(Result);
+	Spin_Button.text = str(Result)
+	print(NumberEntered)
+	
 	return Result
